b196d6c787cf10f98eb917ef51cbed2440ef7f33
Kalyani Bhintade <kalayani.bhintade14@gmail.com>
2025-11-01 18:10:48 +0530

    built teams and hierarchy webpage

---
+++ dsa-project-backend/src/main/java/com/taskmate/dsaprojectbackend/CreateTeamRequest.java
@@ -0,0 +1,34 @@
+package com.taskmate.dsaprojectbackend;
+
+import java.util.List;
+
+public class CreateTeamRequest {
+    private String teamName;
+    private List<Integer> memberIds;
+    private Integer leadId;
+
+    // Getters and Setters
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public List<Integer> getMemberIds() {
+        return memberIds;
+    }
+
+    public void setMemberIds(List<Integer> memberIds) {
+        this.memberIds = memberIds;
+    }
+
+    public Integer getLeadId() {
+        return leadId;
+    }
+
+    public void setLeadId(Integer leadId) {
+        this.leadId = leadId;
+    }
+}
---
+++ dsa-project-backend/src/main/java/com/taskmate/dsaprojectbackend/Team.java
@@ -0,0 +1,61 @@
+package com.taskmate.dsaprojectbackend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;import jakarta.persistence.ManyToMany;import com.fasterxml.jackson.annotation.JsonManagedReference;
+import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.HashSet;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private Integer leadId; // New field to store the ID of the team lead
+
+    @ManyToMany(mappedBy = "teams") // This is the inverse side
+    @JsonManagedReference
+    private Set<Person> members = new HashSet<>();
+
+    public Team() {}
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Person> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Set<Person> members) {
+        this.members = members;
+    }
+
+    public Integer getLeadId() {
+        return leadId;
+    }
+
+    public void setLeadId(Integer leadId) {
+        this.leadId = leadId;
+    }
+}
